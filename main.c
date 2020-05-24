fcee1b4328fea435cfd56d7ee3f111a427b27029
Jose Campos <josigna.cp@gmail.com>
2020-05-24 00:51:08 -0400

    uso de variables globales, lineamientos para las demas operaciones

--- main.c
+++ main.c
@@ -8,48 +8,33 @@
 
 
 
-
-void suma(int *numero);
+int *obtenerNumeros(char ingreso[]);
+void suma();
 void resta();
 void multiplicar();
 void dividir();
+
+int numeros[2];
 
 int main() {
     int x = 1;
     char ingreso[100];
 
-
-
     while(x)
     {
         int i=0;
-        int numeros[2];
+
 
         printf("%s", key);
         scanf("%s", ingreso);
 
-
         printf("el usuario ingreso: %s \n", ingreso);
 
-        char *p  = strtok(ingreso, "+");
-
-        if (p == NULL) {
-            printf("No hay valores, o cadena incorrecta");
-            return -1;
-        }
-        numeros[i++] = atoi(p);
-
-        while (p != NULL)
-        {
-            p = strtok(NULL, "+");
-            if (p != NULL) {
-                numeros[i++] = atoi(p);
-            }
-        }
+        obtenerNumeros(ingreso);
 
         switch('+')
         {
-            case '+': suma(numeros);
+            case '+': suma();
                 break;
         }
     }
@@ -58,13 +43,39 @@ int main() {
     return 0;
 }
 
-void suma(int *numero)
+int *obtenerNumeros(char ingreso[])
 {
-    int size = 2, sum = 0;
+    int i = 0;
+
+    char *p  = strtok(ingreso, "+");
+
+    if (p == NULL) {
+        printf("No hay valores, o cadena incorrecta");
+        return -1;
+    }
+    numeros[i++] = atoi(p);
+
+    while (p != NULL)
+    {
+        p = strtok(NULL, "+");
+        if (p != NULL) {
+            numeros[i++] = atoi(p);
+        }
+    }
+
+    //printf("valor 1: %d ", numeros[0]);
+    //printf("valor 2: %d \n", numeros[1]);
+
+    return numeros;
+}
+
+void suma()
+{
+    int sum = 0;
 
 
-    for (int i = 0; i < size; ++i) {
-        sum += numero[i];
+    for (int i = 0; i < 2; ++i) {
+        sum += numeros[i];
     }
 
     printf("suma total= %d\n", sum);
